8e3a9d67f7e2e9759b8799413c3a30a456de01d5
giamberinigiulia <56107097+giamberinigiulia@users.noreply.github.com>
2022-05-01 16:13:59 +0200

    Macro in avvio.c

--- avvio.c
+++ avvio.c
@@ -1,48 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define MAPPA1 "MAPPA1"
+#define MAPPA2 "MAPPA2"
+#define ETCS1 "ETCS1"
+#define ETCS2 "ETCS2"
+#define RBC "RBC"
+
 void error()
 {
-    printf("Errore nell'inserimento dei parametri\n");
+    printf("\nErrore nell'inserimento dei parametri\n");
 }
 
 
 int main(int argc, char *param[])
 {
-    char *str[] = {"ETCS1","ETCS2","MAPPA1","MAPPA2","RBC"};
+    //char *str[] = {"ETCS1","ETCS2","MAPPA1","MAPPA2","RBC"};
     if(argc<3 || argc>4) error();
-    else if(strcmp(param[1],str[0])==0) //ETCS1
+    else if(strcmp(param[1],ETCS1)==0) //ETCS1
     {
         printf("Modalità %s\n", param[1]);
-        if(strcmp(param[2],str[2])==0) //MAPAP1
+        if(strcmp(param[2],MAPPA1)==0) //MAPPA1
         {
             printf("Mappa %s\n", param[2]);
         }
-        else if(strcmp(param[2],str[3])==0) //MAPPA2
+        else if(strcmp(param[2],MAPPA2)==0) //MAPPA2
         {
             printf("Mappa %s\n", param[2]);
         }
     }
-    else if(strcmp(param[1],str[1])==0) //ETCS2
+    else if(strcmp(param[1],ETCS2)==0) //ETCS2
     {
         printf("Modalità %s\t", param[1]);
-        if(strcmp(param[2],str[2])==0) //MAPPA1
+        if(strcmp(param[2],MAPPA1)==0) //MAPPA1
         {
             printf("\nMappa %s\n", param[2]);
         }
-        else if(strcmp(param[2],str[3])==0) //MAPPA2
+        else if(strcmp(param[2],MAPPA2)==0) //MAPPA2
         {
             printf("\nMappa %s\n", param[2]);
         }
-        else if(strcmp(param[2],str[4])==0) //RBC
+        else if(strcmp(param[2],RBC)==0) //RBC
         {
             printf("%s", param[2]);
             if(argc<4) error();
-            else if(strcmp(param[3],str[2])==0) //MAPPA1
+            else if(strcmp(param[3],MAPPA1)==0) //MAPPA1
             {
                 printf("\nMappa %s\n", param[3]);
             }
-            else if(strcmp(param[3],str[3])==0) //MAPPA2
+            else if(strcmp(param[3],MAPPA2)==0) //MAPPA2
             {
                 printf("\nMappa %s\n", param[3]);
             }
